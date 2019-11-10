d199e5ab46cbd76c57739fcf0f2d26f81f8e16f6
Dan Gallagher <dagallagher221@gmail.com>
2019-11-10 13:03:03 -0500

    added connection, reqs, and error

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,23 @@
 //Add Packages
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require("cli-table");
+
 //Connect to mysql
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "bamazon"
+});
+
 //Create error function in the event sql doesnt connect
+connection.connect(function(err){
+    if(err)throw err;
+    console.log("connected as id" + connection.threadId);
+});
+
 //Create function to display products in node
 //Create function to purchase products and prompt user
 //Create function to act as a receipt
