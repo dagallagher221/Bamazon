75f229c64608ac4e7929726f3891844b03db27f5
Dan Gallagher <dagallagher221@gmail.com>
2019-11-10 14:57:25 -0500

    added customer interface and options, need to test sql

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,5 +19,64 @@ connection.connect(function(err){
 });
 
 //Create function to display products in node
+
+var displayProducts = function() {
+    var query = "Select * FROM products";
+    connection.query(query, function(err, res) {
+        if(err) throw err;
+        var displayTable = new Table ({
+            head: ["Item ID", "Product Name", "Category", "Price", "Quantity"],
+            colWidths: [10, 25, 25, 10, 14]
+        });
+        for (var i = 0; i < res.length; i++) {
+            displayTable.push(
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+            );
+        }
+        console.log(displayTable.toString());
+        purchasePrompt();
+    })
+}
 //Create function to purchase products and prompt user
+
+function purchasePrompt() {
+    inquirer.prompt([
+        {
+                name: "ID",
+                type: "input",
+                message: "Please enter Item ID you like to purchase.",
+                filter:Number
+        },
+        {
+                name: "Quantity",
+                type: "input",
+                message: "How many items do you wish to purchase?",
+                filter:Number
+        },
+    
+    ]).then(function(answers){
+        var quantityNeeded = answers.Quantity;
+        var IDrequested = answers.ID;
+        purchaseOrder(IDrequested, quantityNeeded);
+    });
+};
 //Create function to act as a receipt
+
+function purchaseOrder(ID, amtNeeded) {
+    connnection.query('Select * FROM products WHERE item_id = ' + ID, function(err,res) {
+        if(err){console.log(err)};
+        if(amtNeeded <= res[0].stock_quantity) {
+            var totalCost = res[0].price * amtNeeded;
+            console.log("Your Game is in stock");
+            console.log(res[0].product_name + "    X     " + amtNeeded);
+            console.log("Total            " + totalCost);
+            connection.queryf("UPDATE products SET stock_quantity = stock_quantity - " + amtNeeded + "WHERE item_id = " + ID);
+        } else{
+            console.log("Sorry, Your Game " + res[0].product_name + " is in another Castle!");
+            console.log("Come back soon, our stock is constantly updating!")
+        };
+        displayProducts();
+    });
+};
+
+displayProducts();
